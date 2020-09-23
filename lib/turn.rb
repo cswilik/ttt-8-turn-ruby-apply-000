7c3342e198112a2c8df945c72d7dcc66a61118bc
Chelsey <cswilik@gmail.com>
2020-09-23 15:52:25 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,9 +14,9 @@ end
 
 def valid_move?(board, index)
   if !position_taken?(board, index) && (index).between?(0,8)
-    return false
+    return true
   else
-    return true 
+    return false
   end 
 end 
 
