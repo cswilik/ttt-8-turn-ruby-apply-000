b2c69bc4944b3dc315aa7b5125d84e9a87f02cfb
Chelsey <cswilik@gmail.com>
2020-09-23 15:37:00 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,7 +9,9 @@ end
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil 
     return false 
-    
+  end 
+end 
+
 def valid_move?(board, index, )
 
 
